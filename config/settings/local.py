ec68138355d312b80e9d585334d74ec932f29292
paolopaolopaolo <dpaolomercado@gmail.com>
2016-08-24 23:47:24 -0700

    Project Start: Add general project structure and orchestration

---
+++ config/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "xU6~x@&hq-*P;2P/ua]6nxt8@UCG*?~4.8;Vpk/N"
+
+ALLOWED_HOSTS = ['*']
