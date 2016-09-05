1cc0319ccdc9d5970759b627f3484c5bd321899c
paolopaolopaolo <dpaolomercado@gmail.com>
2016-09-05 12:26:57 -0700

    Add public remote server

---
+++ config/settings/pub.py
@@ -0,0 +1,15 @@
+from .base import *
+import os
+
+DEBUG = False
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = ["vault.dpmercado.com", ]
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
