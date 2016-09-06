477de2a25e267b8800c95e5920e7a3b9c47b6436
paolopaolopaolo <dpaolomercado@gmail.com>
2016-09-05 22:32:51 -0700

    Change endpoints to `game` subdomain

--- config/settings/pub.py
+++ config/settings/pub.py
@@ -5,7 +5,7 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-ALLOWED_HOSTS = ["138.68.59.106", ]
+ALLOWED_HOSTS = ["game.dpmercado.com", ]
 
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
